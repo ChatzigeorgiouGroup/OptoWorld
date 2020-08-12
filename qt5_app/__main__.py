2a2cc856e33d18af90641385aa8fe0867bea4c6e
daniel <danieldondorp@gmail.com>
2020-08-12 14:32:21 +0200

    started qt5 app to control light

---
+++ qt5_app/__main__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 14:23:46 2020
+
+@author: daniel
+"""
+
+
+
+from mainwindow_ui import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import paho.mqtt.client as client
+import sys
+import time
+
+class Light_Switch():
+    def __init__(self, broker_address, client_name,light_state = 0, port = 1883):
+        self.client_name = client_name
+        self.client = client.Client(client_name)
+        self.client.connect(broker_address, port)
+        self.light_state = light_state
+        
+    def switch(self, val):
+        
+        self.client.publish("optoworld/switch", val)
+        
+    def sync_with_world(self):
+        self.switch()
+        time.sleep(0.1)
+        self.switch()
+        
+        
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.switch = Light_Switch("192.168.1.9", "light_switch")
+        
+        self.ui.button_lightswitch.clicked.connect(self.button_clicked)
+        
+        self.ui.dial.setMinimum(0)
+        self.ui.dial.setMaximum(255)
+        self.ui.dial.setValue(0)
+        
+    
+    def button_clicked(self):
+        self.switch.switch(self.ui.dial.value())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    #sys.exit(app.exec_())
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtWidgets.QApplication.instance().exec_()
+    
---
+++ qt5_app/mainwindow_ui.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(305, 397)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.dial = QtWidgets.QDial(self.centralwidget)
+        self.dial.setObjectName("dial")
+        self.verticalLayout.addWidget(self.dial)
+        self.button_lightswitch = QtWidgets.QPushButton(self.centralwidget)
+        self.button_lightswitch.setCheckable(False)
+        self.button_lightswitch.setObjectName("button_lightswitch")
+        self.verticalLayout.addWidget(self.button_lightswitch)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 305, 30))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button_lightswitch.setText(_translate("MainWindow", "Switch the Light"))
+
+
