3c3dcf128a22bcdd6dd2ea71a3f5014c9c5ba5ca
daniel <danieldondorp@gmail.com>
2021-02-12 13:18:03 +0100

    comments in microcontroller code

--- qt5_app/controller_code_mqtt_qt/controller_code_mqtt_qt.ino
+++ qt5_app/controller_code_mqtt_qt/controller_code_mqtt_qt.ino
@@ -1,27 +1,21 @@
 
 
 /*
- Basic ESP8266 MQTT example
+ OptoWorld Microcrontroller code
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
+ This code does the following: 
+ 
  It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
+  - Measures Temperature with a ds18b20 onewire thermometer
+  - Measures light intensity in lux using a bh1750 module
+  - Publishes the values to MQTT topic optoworld/status
+  - subscribes to the topic "optoworld/switch". 
+    Callback on incoming messages for this topic sets the neopixels to the intensity value specified
+    in the message.
+ The code will attempt reconnecting to the server if the connection is lost using a blocking
+ reconnect function. 
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
+ Replace the placeholders for ssid, password and broker ip with your real values.
 
 */
 
