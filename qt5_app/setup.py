8e9bf6f633f979c57a11d0701abca7540ebd7657
daniel <danieldondorp@gmail.com>
2021-02-16 10:57:52 +0100

    added setup.py

---
+++ qt5_app/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qt5_app',
+    version='0.1',
+    packages=[''],
+    url='github.com/chatzigeorgiougroup/optoworld',
+    license='GPL-3',
+    author='daniel',
+    author_email='danieldondorp@gmail.com',
+    description='OptoWorld - hardware and software for optogenetics'
+)
